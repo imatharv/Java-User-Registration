97758533eb77e35557151778814c23b14b550c8c
Atharva Joshi <iamatharvaj@google.com>
2021-05-12 21:45:13 +0530

    Initial commit

---
+++ UserRegistration.java
@@ -0,0 +1,8 @@
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java regex user registration.");
+    }
+}
