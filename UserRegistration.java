09efb67f96646bbec7da298ac37ec5fc5a3a44c8
Atharva Joshi <iamatharvaj@google.com>
2021-05-12 21:58:45 +0530

    Added: Solution for validating email address

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class UserRegistration {
     public static final String FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}";
     public static final String LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})*$";
     public static boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(FIRST_NAME_REGEX);
         return pattern.matcher(firstName).matches();
@@ -12,9 +13,13 @@ public class UserRegistration {
         Pattern pattern = Pattern.compile(LAST_NAME_REGEX);
         return pattern.matcher(lastName).matches();
     }
+    public static boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        return pattern.matcher(email).matches();
+    }
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
-        int fName = 0, lName = 0;
+        int fName = 0, lName = 0, eMail = 0;
         do {
             System.out.println("Enter first name: ");
             String fname = object.nextLine();
@@ -39,5 +44,17 @@ public class UserRegistration {
                 System.out.println("That's an invalid last name.");
             }
         } while(lName == 0);
+        do {
+            System.out.println("Enter email address: ");
+            String email = object.nextLine();
+            if ((validateEmail(email)) == true) {
+                eMail = 1;
+                System.out.println("That's a valid email address.");
+            } 
+            else {
+                eMail = 0;
+                System.out.println("That's an invalid email address.");
+            }
+        } while(eMail == 0);
     }
 }
