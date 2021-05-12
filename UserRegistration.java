adc0f7f79648087c559d630d0349d4d9ba4436f8
Atharva Joshi <iamatharvaj@google.com>
2021-05-12 22:10:07 +0530

    Added: Solution for validating mobile number

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,6 +5,7 @@ public class UserRegistration {
     public static final String FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}";
     public static final String LAST_NAME_REGEX = "^[A-Z]{1}[A-Za-z]{2,}$";
     public static final String EMAIL_REGEX = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[A-Za-z0-9]+.[A-Za-z]{2,4}([.][A-Za-z]{2})*$";
+    public static final String MOBILE_NO_REGEX = "^[+]{0,1}[0-9]{2,3}[: :][7-9]{1}[0-9]{9}$";
     public static boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile(FIRST_NAME_REGEX);
         return pattern.matcher(firstName).matches();
@@ -17,9 +18,13 @@ public class UserRegistration {
         Pattern pattern = Pattern.compile(EMAIL_REGEX);
         return pattern.matcher(email).matches();
     }
+    public static boolean validateMobileNo(String mobileNo) {
+        Pattern pattern = Pattern.compile(MOBILE_NO_REGEX);
+        return pattern.matcher(mobileNo).matches();
+    }
     public static void main(String[] args) {
         Scanner object = new Scanner(System.in);
-        int fName = 0, lName = 0, eMail = 0;
+        int fName = 0, lName = 0, eMail = 0, mobile = 0;
         do {
             System.out.println("Enter first name: ");
             String fname = object.nextLine();
@@ -56,5 +61,17 @@ public class UserRegistration {
                 System.out.println("That's an invalid email address.");
             }
         } while(eMail == 0);
+        do {
+            System.out.println("Enter mobile number: ");
+            String mobileNo = object.nextLine();
+            if ((validateMobileNo(mobileNo)) == true) {
+                mobile = 1;
+                System.out.println("That's a valid mobile number.");
+            } 
+            else {
+                mobile = 0;
+                System.out.println("That's an invalid mobile number.");
+            }
+        } while(mobile == 0);
     }
 }
